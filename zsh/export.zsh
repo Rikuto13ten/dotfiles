11f2e97cfb280366aa70956d5c360ad33b64e923
Rikuto13ten <rikuto1367@gmail.com>
2025-05-06 21:58:37 +0900

    update

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -4,4 +4,8 @@ export PATH="$PATH":"$HOME/Library/Android/sdk/platform-tools"
 # Nixパッケージマネージャーのバイナリパスを追加
 export PATH="/Users/usr/.nix-profile/bin:$PATH"
 
+# SKK 用
 export PATH="$HOME/yaskkserv2/target/release:$PATH"
+
+# Homebrew のパスを通す
+eval "$(/opt/homebrew/bin/brew shellenv)"
